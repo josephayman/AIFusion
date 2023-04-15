39ba78d940c6416b95ee33cbb94462f8b038bba6
Joseph Ayman <josephayman@mail.com>
2023-04-15 02:37:10 +0200

    fix

--- src/pages/api/format-text.ts
+++ src/pages/api/format-text.ts
@@ -13,7 +13,7 @@ export default async function getText(req: NextApiRequest, res: NextApiResponse)
     max_tokens: 2000,
     temperature: 0.6,
   });
-  res.status(200).json({ result: completion.data.choices[0].text || "" });
+  res.status(200).json({ result: completion.data.choices[0]?.text || "" });
 }
 
 function textPrompt(plainText: string) {
